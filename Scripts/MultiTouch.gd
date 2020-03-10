c712a32ad945f2969191a66648d105d8051f222d
daveymclain <braveheart_52@hotmail.com>
2020-03-10 13:35:34 +0000

    Added a smooth stop after panning

--- Scripts/MultiTouch.gd
+++ Scripts/MultiTouch.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 onready var map_node = get_node("/root/App/Map")
 
+export(float) var pan_smooth := -5
+
 var touches = {}
 var touch_active = false
 var current_position = Vector2(0, 0)
 var last_center_pos
+var vel = Vector2(0,0)
 
 
 func _unhandled_input(event):
@@ -23,7 +26,9 @@ func _unhandled_input(event):
 func _process(delta):
 	if touches.size() > 1:
 		update_vel(delta)
-	pass
+	if touches.size() == 0:
+		do_real_smoothing(delta)
+	
 
 func update_touch_info():
 	var avg_touch = Vector2(0, 0)
@@ -50,5 +55,10 @@ func update_vel(delta):
 			last_center_pos = current_position
 		var move = last_center_pos - current_position
 		var move_speed : Vector2 = move / delta
+		vel = move_speed
 
-
+func do_real_smoothing(delta : float):
+	var l = vel.length()
+	var move_frame = 10 * exp(pan_smooth * ((log(l/10) / pan_smooth)+delta))
+	vel = vel.normalized() * move_frame
+	map_node.position -= vel * delta
